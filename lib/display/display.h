49853994068d9799b3b0f40b275df0b60999b318
Reishin-Sakuma <sakumareisinn@gmail.com>
2025-05-28 22:59:43 +0900

    拡張子をhpp→hに変更し内容修正

---
+++ lib/display/display.h
@@ -0,0 +1,9 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void initDisplay();
+void drawUILayout();
+void drawTemperature(float temp);
+void drawSpeed(int spd);
+
+#endif
--- lib/display/display.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef DISPLAY_HPP
-#define DISPLAY_HPP
-
-void initDisplay();
-void displayTemperature(float tempC);
-
-#endif

6bc0ba4af21b560679ccd365c0b7293c0f0b8d05
Reishin-Sakuma <sakumareisinn@gmail.com>
2025-05-28 23:00:59 +0900

    UI表示テスト用コード実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
-#include "display.hpp"
-#include "temp_sensor.hpp"
+#include <display.h>
 
 void setup() {
-  Serial.begin(115200);
   initDisplay();
-  initTempSensor();
 }
 
 void loop() {
-  float tempC = readTemperature();
-  displayTemperature(tempC);
-  delay(2000);
+  float temp = 36.2;  // 仮温度
+  int speed = 42;     // 仮速度
+
+  drawTemperature(temp);
+  drawSpeed(speed);
+
+  delay(2000);  // 2秒ごとに更新
 }

190a40f88c22a255b48853eb7b1f49baf76cb9a7
Reishin-Sakuma <sakumareisinn@gmail.com>
2025-06-01 23:04:09 +0900

    ESP32動作確認用コード

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
+// #include <Arduino.h>
+// #include <display.h>
+
+// void setup() {
+//   initDisplay();
+// }
+
+// void loop() {
+//   float temp = 36.2;  // 仮温度
+//   int speed = 42;     // 仮速度
+
+//   drawTemperature(temp);
+//   drawSpeed(speed);
+
+//   delay(2000);  // 2秒ごとに更新
+// }
+
 #include <Arduino.h>
-#include <display.h>
 
 void setup() {
-  initDisplay();
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("ESP32 起動完了！ようこそ『しゃべるモニター』へ！");
 }
 
 void loop() {
-  float temp = 36.2;  // 仮温度
-  int speed = 42;     // 仮速度
-
-  drawTemperature(temp);
-  drawSpeed(speed);
-
-  delay(2000);  // 2秒ごとに更新
+  Serial.println("動作中…");
+  delay(1000);
 }

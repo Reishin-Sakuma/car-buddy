e2f799ec74fd527d0745827e361823bd7987d3a0
Reishin-Sakuma <sakumareisinn@gmail.com>
2025-06-08 04:04:58 +0900

    元のmain.cppに戻したらm何故か動かないくそ

--- include/ui.hpp
+++ include/ui.hpp
@@ -1,6 +1,9 @@
 #ifndef UI_HPP
 #define UI_HPP
 
+#include <TFT_eSPI.h>
+extern TFT_eSPI tft;  // ← これがないとtftが未定義になる
+
 void drawUI(float speed);
 void drawCharacterImage(int x, int y);
 void drawTemperature(float temp);

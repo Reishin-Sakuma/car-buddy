1302565980762b759066e7778c5fd4dda099d71c
Reishin-Sakuma <sakumareisinn@gmail.com>
2025-06-08 01:06:57 +0900

    温度表示・速度表示位置の若干修正

--- src/ui.cpp
+++ src/ui.cpp
@@ -13,17 +13,17 @@ void drawUI(float speed) {
   tft.setTextSize(3);
 
   // 速度表示（右下）
-  tft.drawString("Speed:\n" + String(speed, 1) + " km/h", 200, 150);
+  tft.drawString("Speed: " , 200, 130);
+  tft.drawString(String(speed, 1), 200, 155);
+  tft.drawString(" km/h" , 200, 180);
 
   // キャラ画像枠（仮）
   tft.drawRect(10, 20, 160, 160, TFT_WHITE);
 }
 
 void drawTemperature(float temp) {
-  tft.setTextDatum(TL_DATUM);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.drawString("Temp:", 200, 10);               // 1行目
-  tft.drawString(String(temp, 1) + " C", 200, 30); // 2行目（Y座標をずらす）
+  tft.drawString(String(temp, 1) + " C", 200, 35); // 2行目（Y座標をずらす）
 }
 
 
